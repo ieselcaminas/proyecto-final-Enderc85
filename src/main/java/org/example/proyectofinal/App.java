7f4dae00d301b8e0271d6e9bdc5e26551e7348fe
Xavi Roca <xaavii.dev@gmail.com>
2026-04-24 12:59:54 +0200

    Boton de cerrar sesión funcional

--- src/main/java/org/example/proyectofinal/App.java
+++ src/main/java/org/example/proyectofinal/App.java
@@ -14,43 +14,42 @@ public class App extends Application {
     private static Scene scene;
     private static User loggedUser;
 
-    private UserService userService;
-    private SerieService serieService;
-    private UserSerieService userSerieService;
-
-    @Override
-    public void init() {
-        userService = Launcher.springContext.getBean(UserService.class);
-        serieService = Launcher.springContext.getBean(SerieService.class);
-        userSerieService = Launcher.springContext.getBean(UserSerieService.class);
-    }
-
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("login.fxml"));
-
-        loader.setControllerFactory(param -> {
-            if (param == LoginController.class) return new LoginController(userService);
-            if (param == MenuController.class) return new MenuController();
-            if (param == SeriesController.class) return new SeriesController(Launcher.springContext.getBean(SerieService.class));
-            if (param == UserSerieController.class) return new UserSerieController(userSerieService);
-            return null;
-        });
-
-        Parent root = loader.load();
-        scene = new Scene(root);
+        scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
+        stage.setTitle("SeenIt");
         stage.show();
     }
 
     public static void setRoot(String fxml) {
         try {
-            FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-            Parent root = loader.load();
-            scene.setRoot(root);
+            scene.setRoot(loadFXML(fxml));
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Parent loadFXML(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+
+        loader.setControllerFactory(param -> {
+            if (param == LoginController.class)
+                return new LoginController(Launcher.springContext.getBean(UserService.class));
+
+            if (param == MenuController.class)
+                return new MenuController();
+
+            if (param == SeriesController.class)
+                return new SeriesController(Launcher.springContext.getBean(SerieService.class));
+
+            if (param == UserSerieController.class)
+                return new UserSerieController(Launcher.springContext.getBean(UserSerieService.class));
+
+            return null;
+        });
+
+        return loader.load();
     }
 
     public static void setUser(User user) {
